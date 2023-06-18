8cb876234043f07a986664cf359dad1a7793d0dd
Atsushi Neki <nekiaiken@gmail.com>
2023-06-18 16:36:47 +0900

    Merge 2 search conditions

--- backup-redis-dump.sh
+++ backup-redis-dump.sh
@@ -2,3 +2,6 @@
 podname=$(kubectl get pod -n rentapp --selector=app=rentapp -o jsonpath="{.items[].metadata.name}")
 echo $podname
 kubectl cp -n rentapp $podname:/data/dump.rdb ./dump-$(date +%Y%m%d%H%M).rdb
+podname2=$(kubectl get pod -n rentapp2 --selector=app=rentapp -o jsonpath="{.items[].metadata.name}")
+echo $podname2
+kubectl cp -n rentapp2 $podname2:/data/dump.rdb ./dump2-$(date +%Y%m%d%H%M).rdb
